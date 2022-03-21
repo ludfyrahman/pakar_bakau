d61aa5df644d1dd50310ff4420b15b8b2a12c501
ludfy rahman <ludfyrahman@ludfys-MacBook-Pro.local>
2022-03-21 22:33:20 +0700

    fix

--- application/views/frontend/content/home/hasil.php
+++ application/views/frontend/content/home/hasil.php
@@ -3,41 +3,51 @@
         <div class="card p-5">
             <img src="<?= base_url('assets/img/hen.png') ?>" class='rounded mx-auto d-block' style="width:300px" alt="">
             <i>Hasil penyakit yang ditampilkan sesuai dengan gejala yang dipilih</i>
-            <table class="table table-striped mt-4">
-                <tr>
-                    <td>Nama Penyakit</td><td><?= $data[0]['penyakit'] ?></td>
-                </tr>
-                <tr>
-                    <td>Gejala</td><td>
-                        <?php 
-                        foreach ($gejala as $nomor => $g) {
-                            echo ($nomor+1).". ".(in_array($g['id'], $input) ? '<b>' : '').$g['nama'].(in_array($g['id'], $input) ? '</b>' : '')."<br>";
-                        }
-                        ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Nilai Probabilitas</td><td><b><?= $data[0]['v'] ?></b></td>
-                </tr>
-            </table>
-            <table class="table table-striped mt-4">
-                <thead>
+            <div class="table-responsive">
+                <table class="table table-striped mt-4">
                     <tr>
-                        <td>NO</td><td>Nama Penyakit</td><td>Nilai V</td>
+                        <td>Nama Penyakit</td><td><?= $data[0]['penyakit'] ?></td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php 
-                    foreach ($data as $index => $d) {
-                    ?>
-                    <tr class='<?= $index == 0 ? 'bg-success text-white' : '' ?>'>
-                        <td><?= $index+1 ?></td>
-                        <td><?= $d['penyakit'] ?></td>
-                        <td><?= $d['v'] ?></td>
+                    <tr>
+                        <td>Gejala</td><td>
+                            <?php 
+                            $total = count($gejala);
+                            $prob = 0;
+                            foreach ($gejala as $nomor => $g) {
+                                echo ($nomor+1).". ".(in_array($g['id'], $input) ? '<b>' : '').$g['nama'].(in_array($g['id'], $input) ? '</b>' : '')."<br>";
+                                if(in_array($g['id'], $input)){
+                                    $prob++;
+                                }
+                            }
+                            ?>
+                        </td>
                     </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+                    <tr>
+                        <td>Nilai Probabilitas</td><td><b><?= $data[0]['v'] ?></b></td>
+                    </tr>
+                    <tr>
+                        <td>Presentase</td><td><?= $prob / $total * 100 ?>%</td>
+                    </tr>
+                </table>
+                <table class="table table-striped mt-4">
+                    <thead>
+                        <tr>
+                            <td>NO</td><td>Nama Penyakit</td><td>Nilai V</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        foreach ($data as $index => $d) {
+                        ?>
+                        <tr class='<?= $index == 0 ? 'bg-success text-white' : '' ?>'>
+                            <td><?= $index+1 ?></td>
+                            <td><?= $d['penyakit'] ?></td>
+                            <td><?= $d['v'] ?></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </main>
--- application/views/frontend/content/home/konsultasi.php
+++ application/views/frontend/content/home/konsultasi.php
@@ -1,23 +1,23 @@
 <main class="main-content mt-1 border-radius-lg">
     <div class="container mt-4">
-        <div class="card p-5">
+        <!-- <div class="card p-5"> -->
             <img src="<?= base_url('assets/img/hen.png') ?>" class='rounded mx-auto d-block' style="width:300px" alt="">
             <i>Silahkan pilih gejala sesuai kondisi  yang ditemukan pada ayam anda</i>
             <form action="<?= base_url('site/hasil') ?>" method="post">
                <div class="table-responsive">
-               <table class="table table-striped mt-4">
+               <table class=" table-striped mt-4">
                     <tr>
-                        <th>Nama Gejala</th><th>Pilih kondisi</th>
+                        <th >Nama Gejala</th><th >Pilih kondisi</th>
                     </tr>
                     <?php foreach ($data as $g) {?>
                     <tr>
-                        <td><?= $g['nama'] ?></td><td><input type="checkbox" value='<?= $g['id'] ?>' name="gejala[]" id=""></td>
+                        <td ><?= $g['nama'] ?></td><td ><input type="checkbox" value='<?= $g['id'] ?>' name="gejala[]" id=""></td>
                     </tr>
                     <?php } ?>
                 </table>
                </div>
                 <button class="btn btn-primary" type="submit">Proses</button>
             </form>
-        </div>
+        <!-- </div> -->
     </div>
 </main>
